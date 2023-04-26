9f3daeaa23595d325511662c5f116bfd443dbeb1
CatfishCrypt <64277025+CatfishCrypt@users.noreply.github.com>
2023-04-26 08:57:56 -0400

    Add files via upload

---
+++ VC_CUDA102/x64/Release/clienttest.bat
@@ -0,0 +1,122 @@
+KangarooWorker.exe -t 4 -wname WanderingPhilosopher -c 52.177.217.235 -sp 4448
+pause
+192.168.29.187
+-c 52.177.217.235 -sp 4448
+-dpcount countfile: Specify file to save running DP count stats into (server only)
+ -dpi countInverval: Periodic interval (in seconds) for saving DP count stats (server only)
+ -wname workerName: Specify worker name, up to 32 alphanumeric or underscore characters max, (client only)
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 36 -WRS 10FF0000000000000000000000000000 -kpw 36 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 40 -WRS 10FF0000000000000000000000000000 -kpw 40 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 44 -WRS 10FF0000000000000000000000000000 -kpw 44 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 48 -WRS 10FF0000000000000000000000000000 -kpw 48 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 52 -WRS 10FF0000000000000000000000000000 -kpw 52 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 56 -WRS 10FF0000000000000000000000000000 -kpw 56 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 60 -WRS 10FF0000000000000000000000000000 -kpw 60 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 64 -WRS 10FF0000000000000000000000000000 -kpw 64 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 68 -WRS 10FF0000000000000000000000000000 -kpw 68 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 72 -WRS 10FF0000000000000000000000000000 -kpw 72 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 76 -WRS 10FF0000000000000000000000000000 -kpw 76 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 80 -WRS 10FF0000000000000000000000000000 -kpw 80 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 84 -WRS 10FF0000000000000000000000000000 -kpw 84 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 88 -WRS 10FF0000000000000000000000000000 -kpw 88 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 92 -WRS 10FF0000000000000000000000000000 -kpw 92 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 96 -WRS 10FF0000000000000000000000000000 -kpw 96 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 100 -WRS 10FF0000000000000000000000000000 -kpw 100 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 104 -WRS 10FF0000000000000000000000000000 -kpw 104 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 108 -WRS 10FF0000000000000000000000000000 -kpw 108 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 112 -WRS 10FF0000000000000000000000000000 -kpw 112 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
+
+
+
+
+
+goto :while1
+
+start /max KangarooWTFSolo1.exe -t 0 -TRS 10F00000000000000000000000000000 -kpt 116 -WRS 10FF0000000000000000000000000000 -kpw 116 -gpu -gpuId 0,1,2,3,4,5 -g 84,64,84,64,84,64,84,64,84,64,84,64 -c 192.168.29.187 -sp 4349
+timeout /t 300 /nobreak
+taskkill /im KangarooWTFSolo1.exe /f
+timeout /t 4 /nobreak
